ef0920da626f624f3b64a63bb3344737f95b4a7c
nununugit <hiraganadenu@gmail.com>
2021-04-26 15:21:39 +0900

    add: 合否、順位、記録換算点の追加及びにソートの実装

--- file_output.c
+++ file_output.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define NUM 7
-#define DTCNT  ((sizeof res)/(sizeof(res_t)))
+#define NUM 13
    struct score {
-    int number;
-    int japanese;
-    int math;
-    int english;
+    int number; //受験番号
+    int japanese_score; //国語のテスト点
+    int math_score; //数学のテスト点
+    int english_score; //英語のテスト点
+
+    int japanese_grade; //国語の成績
+    int ss_grade; // 社会の成績(social studies)
+    int math_grade; //数学の成績
+    int science_grade; //理科の成績
+    int music_grade; //音楽の成績
+    int art_grade; //美術の成績
+    int pe_grade; //体育の成績
+    int helt_grade; //技術家庭 (home economics life technique)
+    int english_grade; //英語の成績
+
+    int total_score; //テスト点数の合計
+    int total_grade; //内申点の合計
+    float base_score; //記録換算点(内申点を点数化したもの)
+
+    float score; //全てを考慮した点数
+
+    int result; //合否
+    int rank; //順位
   };
 
 void score_sort(struct score scores[]){
@@ -15,7 +33,7 @@ void score_sort(struct score scores[]){
    struct score tmp;
     for(s=0 ; s<NUM-1 ; s++){
         for(t=s+1;t<NUM;t++){
-            if(scores[t].japanese>scores[s].japanese){
+            if(scores[t].score>scores[s].score){
                 tmp = scores[t];
                 scores[t] = scores[s];
                 scores[s] = tmp;
@@ -25,16 +43,23 @@ void score_sort(struct score scores[]){
     }
 }
 
-void clac_score(struct score scores[]){
-
+int avg_score(struct score scores[],int ninzu){
+  int i;
+  float avg;
+  for(i=0;i<ninzu;i++){
+    avg += scores[i].score;
+  }
+  avg = avg/i;
+  return avg;
 }
 
 int main(){
   FILE *fp;
   FILE *fp_out;
-  char *fname = "input_test.csv";
-  int ret,i=0;
-  char buf[NUM][10];
+
+  char *fname = "input.csv";
+  int ret , i=0 ,avg;
+  char buf[NUM][NUM];
   int data[NUM];
   char fname_out[24];
 
@@ -47,45 +72,89 @@ int main(){
 
   printf("\n");
   
-  fscanf(fp, "%[^,],%[^,],%[^,],%s", buf[0], buf[1], buf[2], buf[3]);
-  printf("%s %s %s %s \n",buf[0], buf[1], buf[2], buf[3]);
+  fscanf(fp, "%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%s", buf[0], buf[1], buf[2], buf[3],buf[4],buf[5],buf[6],buf[7],buf[8],buf[9],buf[10],buf[11],buf[12]);
+  
+  // printf("%s %s %s %s \n",buf[0], buf[1], buf[2], buf[3]);
 
   struct score scores[NUM];
-  while((ret=fscanf(fp, "%d, %d, %d, %d", &data[0], &data[1], &data[2], &data[3])) != EOF){
+
+  while((ret=fscanf(fp, "%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d ,%d", &data[0], &data[1], &data[2], &data[3], &data[4], &data[5], &data[6], &data[7], &data[8], &data[9], &data[10], &data[11], &data[12])) != EOF){
     scores[i].number =data[0];
-    scores[i].japanese =data[1];
-    scores[i].math = data[2];
-    scores[i].english =data[3];
+    scores[i].japanese_score =data[1];
+    scores[i].math_score = data[2];
+    scores[i].english_score =data[3];
+
+    scores[i].japanese_grade =data[4];
+    scores[i].ss_grade =data[5];
+    scores[i].math_grade =data[6];
+    scores[i].science_grade =data[7];
+    scores[i].music_grade =data[8];
+    scores[i].art_grade =data[9];
+    scores[i].pe_grade =data[10];
+    scores[i].helt_grade =data[11];
+    scores[i].english_grade =data[12];
+
+    scores[i].total_score = data[1]+data[2]+data[3];
+    scores[i].total_grade =data[4]+data[5]+data[6]+data[7]+data[8]+data[9]+data[10]+data[11]+data[12];
+    scores[i].base_score =(data[4]+data[5]+(data[6]*1.4)+data[7]+data[8]+data[9]+data[10]+data[11]+(data[12]*1.4))*14.29;
+    
+    scores[i].score = scores[i].total_score + scores[i].base_score;
     ++i;
   }
-    clac_score(scores);
-    score_sort(scores);
 
-  for ( i = 0; i < NUM; i++){
-    printf("%d\t",scores[i].number);
-    printf("%d\t",scores[i].japanese );
-    printf("%d\t",scores[i].math );
-    printf("%d\t\n",scores[i].english );
-  }
+  int ninzu = i;
+  
+  avg = avg_score(scores,ninzu);
+  score_sort(scores);
+
+
 
   int year = 2021 ,month = 4;
   sprintf( fname_out, "%04d%02dresult.csv", year, month);
   
   // ファイルのオープン
   if( (fp_out = fopen( fname_out, "w" )) == NULL ) {
-      printf( "結果ファイルがオープンできませんでした\n" );
+      printf( "結果ファイルがオープンできませんでした\n");
       exit( 1 );
   }
   
   // ヘッダ行を出力する
-  fprintf( fp_out, "number,japanese,math,English\n" );
+  fprintf( fp_out, "number , japanese_score , math_score,English_score, japanese_grade,ss_grade,math_grade, science_grade, music_grade , art_grade , pe_grade , helt_grade, english_grade , total_score , total_grade , base_score, score , result,rank\n" );
   
   // 各データをカンマ区切りで出力する
-  for( i = 0; i < NUM; i++ ) {
-      fprintf( fp_out, "%d,%d,%d,%d\n",
-          scores[i].number, scores[i].japanese, scores[i].math, scores[i].english );
-  }
+
+   for( i = 0; i < ninzu; i++ ) {
+     if(scores[i].score>=avg){
+         scores[i].result = 1;
+     }else{
+         scores[i].result = 0;
+     }
+        scores[i].rank = i+1;
+       fprintf( fp_out, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%5.2f,%5.2f,%d,%d\n",
+         scores[i].number, 
+         scores[i].japanese_score, 
+         scores[i].math_score, 
+         scores[i].english_score,
+         scores[i].japanese_grade,
+         scores[i].ss_grade,
+         scores[i].math_grade,
+         scores[i].science_grade,
+         scores[i].music_grade,
+         scores[i].art_grade,
+         scores[i].pe_grade,
+         scores[i].helt_grade,
+         scores[i].english_grade,
+         scores[i].total_score,
+         scores[i].total_grade,
+         scores[i].base_score,
+         scores[i].score,
+         scores[i].result,
+         scores[i].rank
+         );
+   }
 
   printf( "%s に結果が出力されました\n", fname_out );
+
+  printf("%d",avg_score(scores,ninzu));
   fclose( fp_out );
 }
